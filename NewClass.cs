c6d24f3b28e459bc0a241524e97afe9309d51941
Galaktor1 <dementijserg@yandex.ru>
2025-11-23 18:47:58 +0400

    Новая ветка

---
+++ NewClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTwo
+{
+    public class NewClass
+    {
+        public NewClass()
+        {
+            Console.WriteLine("Команда разработчиков 1");
+        }
+    }
+}
